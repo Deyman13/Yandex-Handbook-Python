fad0c42b7e5619122db7fa110e52c2dad68051d1
Daniil <desmond229@mail.ru>
2023-01-03 15:04:24 +0300

    Строки, кортежи, списки

---
+++ Tuples_Lists/str.capitalize.py
@@ -0,0 +1,6 @@
+# Метод	str.capitalize()
+# Описание	Возвращает копию строки, у которой первая буква заглавная, а остальные приведены к строчным
+s = "hello, World!"
+string = s.capitalize()
+print(string)
+# Результат	Hello, world!
---
+++ Tuples_Lists/str.isalpha.py
@@ -0,0 +1,8 @@
+# Метод	str.isalpha()
+# Описание	Возвращает True, если все символы строки являются буквами и в строке есть хотя бы один символ. 
+# Иначе возвращает False
+s = "Letters"
+print(s.isalpha())
+s = "Letters123"
+print(s.isalpha())
+# Результат	True, False
