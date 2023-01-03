4bfb89963348aa60b472e1a04ce3a3ea47072cd5
Daniil Shklyaev <118282451+Deyman13@users.noreply.github.com>
2023-01-03 17:53:07 +0300

    Delete If_Else directory

--- If_Else/if3.py
+++
@@ -1,8 +0,0 @@
-print("Введите первую и последнюю буквы русского алфавита.")
-first_letter = input()
-last_letter = input()
-if (first_letter == "а" or first_letter == "А") and ( # Если первая буква равна а или А и при этом
-        last_letter == "я" or last_letter == "Я"): # Последняя буква равна я или Я то:
-    print("Верно.") 
-else: # Иначе это: 
-    print("Неверно.")
