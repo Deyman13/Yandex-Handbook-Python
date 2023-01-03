e8c1968a382cc6a5e17b3d57fed3681cd8270d85
Daniil Shklyaev <118282451+Deyman13@users.noreply.github.com>
2023-01-03 18:05:16 +0300

    Delete Theory/Numbers directory

--- Theory/Numbers/numbers2.py
+++
@@ -1,9 +0,0 @@
-print("Это калькулятор, он позволит найти сумму чисел, которые вы введете ниже")
-print("Введите целочисленное значение (int)")
-int_number = int(input())
-print("Введите вещественное значение (float)")
-float_number = float(input())
-summa = int_number + float_number
-
-print(f"Вот ваша сумма: {summa}.")
-print("Спасибо за использование нашего калькулятора")
